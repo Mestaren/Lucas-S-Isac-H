92b7ffaba2893ea474dbe4f727dda476502e5e23
Mestaren <lucas.svensson@elev.ga.lbs.se>
2021-10-01 18:43:38 +0200

    wallRun left

--- Lucas-S/Assets/scripts/playerMovement.cs
+++ Lucas-S/Assets/scripts/playerMovement.cs
@@ -17,10 +17,15 @@ public class playerMovement : MonoBehaviour
     public Transform wallCheck;
     public float wallDistance = 0.4f;
     public LayerMask wallMask;
-    public float z;
-    public GameObject FPplayer;
+
+    public Transform wallChackLeft;
+    public float walldistanceLeft = 0.4f;
+
+   // public float z;
+   // public GameObject FPplayer;
 
     bool isWalled;
+    bool isWalledLeft;
 
     Vector3 velocity;
     bool isGrounded;
@@ -35,12 +40,19 @@ public class playerMovement : MonoBehaviour
         
         isWalled = Physics.CheckSphere(wallCheck.position, wallDistance, wallMask);
 
-        if(isGrounded && velocity.y < 0)
+        isWalledLeft = Physics.CheckSphere(wallChackLeft.position, walldistanceLeft, wallMask);
+
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
 
         if (isWalled && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (isWalledLeft && velocity.y < 0)
         {
             velocity.y = -2f;
         }
@@ -82,6 +94,15 @@ public class playerMovement : MonoBehaviour
         {
             gravity = -60f;
           //GetComponent<Transform>().rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (isWalledLeft && !isGrounded)
+        {
+            gravity = -55f;
+        }
+        else
+        {
+            gravity = -60f;
         }
 
         //gravity = -60f + (56f * System.Convert.ToSingle(isWalled && !isGrounded));
