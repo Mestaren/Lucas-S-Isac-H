9f6259daa52d026372d3664eb71f327f034741c1
Mestaren <lucas.svensson@elev.ga.lbs.se>
2021-10-03 19:25:22 +0200

    JetPack part 3
    part 1 och 2 go to trash
    destroy and rebuild :)

--- Lucas-S/Assets/scripts/JetPack.cs
+++ Lucas-S/Assets/scripts/JetPack.cs
@@ -4,38 +4,22 @@ using UnityEngine;
 
 public class JetPack : MonoBehaviour
 {
-    public float maxFuel = 4f;
-    public float thrustForce = 50f;
-    public Rigidbody rigid;
-    public Transform groundedTransform;
-    public ParticleSystem effect;
-
-    private float curFuel;
+    CharacterController CC;
+    playerMovement PM;
 
     void Start()
     {
-        curFuel = maxFuel;
+        CC = (CharacterController)GetComponent<CharacterController>();
+        PM = (playerMovement)GetComponent<playerMovement>();
     }
 
 
     void Update()
     {
-        if (Input.GetAxis("Jump") > 0f && curFuel > 0f)
+      if (Input.GetKey(KeyCode.F))
         {
-            curFuel -= Time.deltaTime;
-            rigid.AddForce(rigid.transform.up * thrustForce, ForceMode.Impulse);
-            effect.Play();
-
-            Debug.Log("fly");
-        }
-        else if(Physics.Raycast(groundedTransform.position, Vector3.down, 50f, LayerMask.GetMask("ground")) && curFuel < maxFuel)
-        {
-            curFuel += Time.deltaTime;
-            effect.Stop();
-        }
-        else
-        {
-            effect.Stop();
+            Vector3 velocity = new Vector3(0, 10, 0);
+            PM.velocity(velocity);
         }
     }
 }
